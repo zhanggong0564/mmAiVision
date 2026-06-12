a473ee2259fad262f6117a517242071ab2dbc61d
zhanggong <zhanggong1@@sungrowpower.com>
2026-06-12 10:38:35 +0800

    style(utils): 格式化训练入口参数解析

--- tools/train.py
+++ tools/train.py
@@ -13,19 +13,11 @@ def parse_args():
     parser = argparse.ArgumentParser(description='Train a detector')
     parser.add_argument('config', help='train config file path')
     parser.add_argument('--work-dir', help='the dir to save logs and models')
+    parser.add_argument('--amp', action='store_true', default=False, help='enable automatic-mixed-precision training')
+    parser.add_argument('--auto-scale-lr', action='store_true', help='enable automatically scaling LR.')
     parser.add_argument(
-        '--amp',
-        action='store_true',
-        default=False,
-        help='enable automatic-mixed-precision training')
-    parser.add_argument(
-        '--auto-scale-lr',
-        action='store_true',
-        help='enable automatically scaling LR.')
-    parser.add_argument(
-        '--resume',
-        action='store_true',
-        help='resume from the latest checkpoint in the work_dir automatically')
+        '--resume', action='store_true', help='resume from the latest checkpoint in the work_dir automatically'
+    )
     parser.add_argument(
         '--cfg-options',
         nargs='+',
@@ -35,12 +27,9 @@ def parse_args():
         'be overwritten is a list, it should be like key="[a,b]" or key=a,b '
         'It also allows nested list/tuple values, e.g. key="[(a,b),(c,d)]" '
         'Note that the quotation marks are necessary and that no white space '
-        'is allowed.')
-    parser.add_argument(
-        '--launcher',
-        choices=['none', 'pytorch', 'slurm', 'mpi'],
-        default='none',
-        help='job launcher')
+        'is allowed.',
+    )
+    parser.add_argument('--launcher', choices=['none', 'pytorch', 'slurm', 'mpi'], default='none', help='job launcher')
     parser.add_argument('--local_rank', type=int, default=0)
     args = parser.parse_args()
     if 'LOCAL_RANK' not in os.environ:
@@ -64,35 +53,32 @@ def main():
         cfg.work_dir = args.work_dir
     elif cfg.get('work_dir', None) is None:
         # use config filename as default work_dir if cfg.work_dir is None
-        cfg.work_dir = osp.join('./work_dirs',
-                                osp.splitext(osp.basename(args.config))[0])
+        cfg.work_dir = osp.join('./work_dirs', osp.splitext(osp.basename(args.config))[0])
 
     # enable automatic-mixed-precision training
     if args.amp is True:
         optim_wrapper = cfg.optim_wrapper.type
         if optim_wrapper == 'AmpOptimWrapper':
-            print_log(
-                'AMP training is already enabled in your config.',
-                logger='current',
-                level=logging.WARNING)
+            print_log('AMP training is already enabled in your config.', logger='current', level=logging.WARNING)
         else:
             assert optim_wrapper == 'OptimWrapper', (
                 '`--amp` is only supported when the optimizer wrapper type is '
-                f'`OptimWrapper` but got {optim_wrapper}.')
+                f'`OptimWrapper` but got {optim_wrapper}.'
+            )
             cfg.optim_wrapper.type = 'AmpOptimWrapper'
             cfg.optim_wrapper.loss_scale = 'dynamic'
 
     # enable automatically scaling LR
     if args.auto_scale_lr:
-        if 'auto_scale_lr' in cfg and \
-                'enable' in cfg.auto_scale_lr and \
-                'base_batch_size' in cfg.auto_scale_lr:
+        if 'auto_scale_lr' in cfg and 'enable' in cfg.auto_scale_lr and 'base_batch_size' in cfg.auto_scale_lr:
             cfg.auto_scale_lr.enable = True
         else:
-            raise RuntimeError('Can not find "auto_scale_lr" or '
-                               '"auto_scale_lr.enable" or '
-                               '"auto_scale_lr.base_batch_size" in your'
-                               ' configuration file.')
+            raise RuntimeError(
+                'Can not find "auto_scale_lr" or '
+                '"auto_scale_lr.enable" or '
+                '"auto_scale_lr.base_batch_size" in your'
+                ' configuration file.'
+            )
 
     cfg.resume = args.resume
 
